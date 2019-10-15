410090f2d86330696ebf581f1af6f9224307011a
Cardoso-CHM <Henrique.cesar100@yahoo.com.br>
2019-10-15 14:27:02 -0300

    Compatibilidade de horarios

--- dev/fonts/teste-global.py
+++ dev/fonts/teste-global.py
@@ -52,6 +52,7 @@ def decodificarValoresDaColuna(df, colunas, indices):
             
 #carregando indices
 idx = gerarDictIndice("../csv/indice.csv")
+#print(idx)
 #carregando csv de  teste
 df = carregarCsvEmDataframe("../csv/Basico_MG.csv")
 #colocando caracteres das colunas para minusculo
@@ -61,7 +62,7 @@ df.columns = toLowerCase(df.columns)
 del_cols = ['cod_uf', 'nome_da_uf', 'cod_meso', 'nome_da_meso',
        'cod_micro', 'nome_da_micro', 'cod_rm', 'nome_da_rm', 'cod_municipio',
        'nome_do_municipio', 'cod_distrito',
-       'cod_subdistrito', 'nome_do_subdistrito', 'cod_bairro',
+       'cod_subdistrito', 'nome_do_subdistrito','nome_do_distrito', 'cod_bairro',
        'nome_do_bairro', 'var01', 'var02',
        'var03', 'var04', 'var05', 'var06', 'var07', 'var08', 'var09', 'var10',
        'var11', 'var12', 'var13']
@@ -73,4 +74,4 @@ df.columns = decodificarColunasDeDataframe("basico", df, idx)
 
 #decodificando valores das colunas que restaram
 df = decodificarValoresDaColuna(df, df.columns, idx)
-print(df.head())
+print(df)
