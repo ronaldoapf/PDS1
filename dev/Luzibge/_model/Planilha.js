fb52ff7d510c7551ec3573ed9db031054d13c58e
JoaoVitorPereiraUFU <54077296+JoaoVitorPereiraUFU@users.noreply.github.com>
2019-11-25 10:43:51 -0300

    correcao campo filtrar

--- dev/Luzibge/_model/Planilha.js
+++ dev/Luzibge/_model/Planilha.js
@@ -7,6 +7,7 @@ class Planilha {
         this.colunas_selecionadas = col_selecionas;
         this.getRelation = function(){
             let arr = {};
+            let i = 0
             for(i=0;i<this.colunas.length;i++){
                 arr[this.colunas[i]] = i;
                 arr[this.colunas_decodificadas[i]] = i;
