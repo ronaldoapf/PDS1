a03bee9a83a454e5a773f2635068cb8a65273d2b
Cardoso-CHM <Henrique.cesar100@yahoo.com.br>
2019-12-07 13:53:04 -0300

    Listagem de planilhas selecionadas na telaArquivos.html

--- dev/Luzibge/_linkers/files.js
+++ dev/Luzibge/_linkers/files.js
@@ -1,22 +1,20 @@
-$(document).ready(function () {
-    $("#form").submit(function (){
+$(document).ready(function() {
+    $("#form").submit(function() {
         var planilhasCSV = $('#planilhasCSV').prop("files")
         var planilhas = $.map(planilhasCSV, function(val) { return val.path; });
 
-        var indiceCSV =   $('#indiceCSV').prop("files")
+        var indiceCSV = $('#indiceCSV').prop("files")
         var indice = $.map(indiceCSV, function(val) { return val.path; });
 
-        planilhas.forEach( (p,i) => {
+        planilhas.forEach((p, i) => {
             console.log(p)
-            $("#form").append('<input type="hidden" name="planilha'+ i +'" value="'+ p +'">')
+            $("#form").append('<input type="hidden" name="planilha' + i + '" value="' + p + '">')
         })
-        indice.forEach( i => {
+        indice.forEach(i => {
             console.log(i)
-            $("#form").append('<input type="hidden" name="indice" value="'+ i +'">')
+            $("#form").append('<input type="hidden" name="indice" value="' + i + '">')
         })
 
         //return false;
     })
 })
-
-  
