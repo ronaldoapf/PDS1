28155ac35048bdb419614ce8a9f3fefc5542c712
Cardoso-CHM <Henrique.cesar100@yahoo.com.br>
2019-12-07 15:18:54 -0300

    Correção de bugs na listagem e layout

--- dev/Luzibge/_linkers/files.js
+++ dev/Luzibge/_linkers/files.js
@@ -1,4 +1,54 @@
+//funcao para verificar se um item "li" já existe na lista-planilhas
+var check = function(li) {
+    return $("#lista-planilhas li").filter(function(i, li2) {
+        let aux = $(li2).text()
+        aux = aux.replace("remover", "").trim()
+
+        return aux == li;
+    }).length > 0;
+};
+
 $(document).ready(function() {
+
+    //evento de remover um item da lista
+    $("#div-planilhas").on("click", ".remover-planilha", function() {
+        let li = $(this).closest("li")
+        li.remove()
+
+        if ($('#lista-planilhas li').length == 0) $("#div-planilhas").hide()
+
+    });
+
+    //evento quando uma ou varias planilhas São selecionadas no input
+    $('.input-file-planilhas').change(function(e) {
+
+        $("#div-planilhas").show()
+
+        var fileName = ' '
+
+        size = e.target.files.length
+        size--
+
+        for (i = 0; i <= size; i++) {
+            fileName = e.target.files[i].path
+            console.log(">" + check(fileName))
+            if (!check(fileName))
+                $("#lista-planilhas").append(`<li class="list-group-item">${fileName} <button type="button" class="remover-planilha" style="float:right">remover</button> </li>`)
+            else
+                alert(`A planilha "${fileName}" já foi escolhida!`)
+        }
+
+        //$('.custom-file-planilha').html(fileName);
+    });
+
+    $('.input-file-indice').change(function(e) {
+        fileName = e.target.files[0].name
+
+        $('.custom-file-indice').html(fileName)
+
+    })
+
+    //evento antes de submeter o form para outra página
     $("#form").submit(function() {
         var planilhasCSV = $('#planilhasCSV').prop("files")
         var planilhas = $.map(planilhasCSV, function(val) { return val.path; });
